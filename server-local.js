4f81b7769573a94a32525428ee0b8b86fa655bb1
starshine8859 <starshine8859@gmail.com>
2025-09-15 10:49:58 -0400

    Update server setting

--- server-local.js
+++ server-local.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:80', 'http://127.0.0.1:80'],
     credentials: true
 }));
 
